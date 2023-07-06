42dc6e5c7f1cba9d34a886a045b31e5de2e4a398
Błażej Miśkiewicz <blamis@microsoft.com>
2023-07-06 15:17:44 +0200

    change info about time

--- ADE.Tutorial/GetTime.cs
+++ ADE.Tutorial/GetTime.cs
@@ -34,7 +34,7 @@ namespace ADE.Tutorial
 
             var now = DateTime.UtcNow;
 
-            response.WriteString($"UTC Time test6: {now}");
+            response.WriteString($"UTC Time test7: {now}");
 
             foreach (var timeZone in timeZones)
                 response.WriteString($"{(timeZone.IsDaylightSavingTime(now) ? timeZone.DaylightName : timeZone.StandardName)}: {TimeZoneInfo.ConvertTimeFromUtc(now, timeZone)}");
